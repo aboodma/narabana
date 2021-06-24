d54590a515b622d436acf977d573dd7e4c1de70b
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2021-06-24 13:05:35 +0300

    Update Wallet.php

--- app/Wallet.php
+++ app/Wallet.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
+    protected $appends  = ["created_at_human"];
+    public function getCreatedAtHumanAttribute(){
+        \Carbon\Carbon::setLocale('en');
+        return \Carbon\Carbon::createFromTimeStamp(strtotime($this->attributes['created_at']))->diffForHumans() ;
+    }
     /**
      * Get the user that owns the Wallet
      *
