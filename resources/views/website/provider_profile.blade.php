58359f56ac3d79589236222d1cbe03ace990951b
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2021-07-02 15:26:19 +0300

    Update provider_profile.blade.php

--- resources/views/website/provider_profile.blade.php
+++ resources/views/website/provider_profile.blade.php
@@ -27,8 +27,7 @@
         
         <div class="row">
            
-            <h2 class=" d-inline d-none d-md-block d-lg-block">{{$provider->user->name}} <i class="fas fa-check-circle text-primary" style="font-size: large;"></i>
-            </h2>
+           
             <div class="col-md-3">
                 <img style="position: absolute;
                 width: 30%;
@@ -51,7 +50,8 @@
             <div class="col-md-6 mt-4">
                 <div class="row">
                     <div class="col-md-12">
-                        
+                        <h2 class=" d-inline d-none d-md-block d-lg-block">{{$provider->user->name}} <i class="fas fa-check-circle text-primary" style="font-size: large;"></i>
+                        </h2>
                         <span class="pb-2 mb-2">{{_ti($provider->Country->name)}} / {{_ti($provider->ProviderType->name)}}</span>
                         <p class="pt-2">{{$provider->about_me}}</p>
                         <p style="font-weight: bold;color: #ba6089;"><i class="fa fa-clock-o" style="color: #ba6089;font-size: initial;"></i> {{__('Replies in 5 days')}}</p>
