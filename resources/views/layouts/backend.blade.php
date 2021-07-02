fde711d0b18a88ee622c8fcb96386789642cbabb
Abdulrahman Mardinli <34406204+aboodma@users.noreply.github.com>
2021-07-02 16:43:41 +0300

    Update backend.blade.php

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -741,14 +741,14 @@ License: You must have a valid license purchased only from themeforest(the above
 							<!--begin::Copyright-->
 							<div class="text-dark order-2 order-md-1">
 								<span class="text-muted font-weight-bold mr-2">2021©</span>
-								<a href="http://keenthemes.com/metronic" target="_blank" class="text-white text-hover-primary">Keenthemes</a>
+								<a href="#" target="_blank" class="text-white text-hover-primary">Totil</a>
 							</div>
 							<!--end::Copyright-->
 							<!--begin::Nav-->
 							<div class="nav nav-dark order-1 order-md-2">
-								<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pr-3 pl-0 text-muted text-hover-primary">About</a>
-								<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link px-3 text-muted text-hover-primary">Team</a>
-								<a href="http://keenthemes.com/metronic" target="_blank" class="nav-link pl-3 pr-0 text-muted text-hover-primary">Contact</a>
+								<a href="#" target="_blank" class="nav-link pr-3 pl-0 text-muted text-hover-primary">About</a>
+								<a href="#" target="_blank" class="nav-link px-3 text-muted text-hover-primary">Team</a>
+								<a href="#" target="_blank" class="nav-link pl-3 pr-0 text-muted text-hover-primary">Contact</a>
 							</div>
 							<!--end::Nav-->
 						</div>
